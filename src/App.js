4a9453bb9627a65ab55a1437870c0dfd035f1362
Roman Danilov <r.danylov@gmail.com>
2020-05-25 14:30:34 +0300

    init queries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,78 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useQuery } from '@apollo/react-hooks'
+import { gql } from 'apollo-boost'
+
+
+const text = ''
+
+const GET_DATA = gql`
+    query getData {
+      data {
+        done
+        id
+        text
+      }
+    }
+` 
+// const ADD_DATA = gql`
+//     mutation addData($text: String!) {
+//       insert_data(objects: {text: $text) {
+//         returning {
+//           text
+//           id
+//           done
+//         }
+//       }
+//     }
+// `
+
+
+// client.query({
+//   query: gql`
+//     query getData {
+//       data {
+//         done
+//         id
+//         text
+//       }
+//     }
+//   `
+// }).then(data => console.log(data.data.data[0]))
+
+
+function addData(e) {
+  e.preventDefault();
+  // this.text = e.target.value;
+  // this.useQuery(ADD_DATA(this.text))
+}
 
 function App() {
+
+
+  const { data, loading, error } = useQuery(GET_DATA)
+
+
+  if (loading) return (<div>Loading...</div>)
+  if (error) return <div>Error fetching data</div>
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="vh-100 code flex flex-column items-center bg-purple white pa3 fl-1">
+      <h1 className="f2-l">GraphQl Data {" "} <span role="img" aria-label="Checkmark"> !</span></h1>
+      <form onSubmit={addData} className="mb3">
+        <input type="text" placeholder='add Text' className="p2 f4 b--dashed"/>
+        <button type="submit" className="pa2 f4 bg-green"> Add data </button>
+      </form>
+      <hr />
+      <h2>Data list</h2>
+      <div className="flex items-center justify-center flex-column">
+        {data.data.map((item) => (
+          <p key={item.id}><span className="pointer list p1 f3">{item.text}</span>
+            <button className="bg-transparent bn f4 "> <span className="red pointer">  &times; </span></button>
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import ApolloClient, { gql } from 'apollo-boost'
+import { ApolloProvider } from '@apollo/react-hooks'
+
+const client = new ApolloClient({
+  uri: 'https://react-apollo-graph.herokuapp.com/v1/graphql'
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
